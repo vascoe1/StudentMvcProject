46dbc81b03aa31a022ef85b679a61c9cc937ebfa
vascoe1 <ugafilms@gmail.com>
2021-06-09 05:05:47 -0400

    work is ready

---
+++ src/main/java/Student/StudentMVCProject/StudentMvcProjectApplication.java
@@ -0,0 +1,13 @@
+package Student.StudentMVCProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMvcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMvcProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Student/StudentMVCProject/StudentMvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Student.StudentMVCProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMvcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
